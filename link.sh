acc64b62072d53832f42aef66e34ffcbd1036081
Elliott Zheng <admin@hypercube.top>
2020-03-02 11:22:55 +0800

    simple merge

---
+++ link.sh
@@ -0,0 +1,8 @@
+yarn link @opentranslate/languages
+yarn link @opentranslate/translator
+yarn link @opentranslate/caiyun
+yarn link @opentranslate/baidu
+yarn link @opentranslate/google
+yarn link @opentranslate/sogou
+yarn link @opentranslate/tencent
+yarn link @opentranslate/youdao
