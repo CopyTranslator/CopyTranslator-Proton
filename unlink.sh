81f35de7ab4b1bfcfdbb87736e5706386ff753ea
Elliott Zheng <admin@hypercube.top>
2020-04-04 10:14:02 +0800

    intergate focus into contrast

---
+++ unlink.sh
@@ -0,0 +1,8 @@
+yarn unlink @opentranslate/languages
+yarn unlink @opentranslate/translator
+yarn unlink @opentranslate/caiyun
+yarn unlink @opentranslate/baidu
+yarn unlink @opentranslate/google
+yarn unlink @opentranslate/sogou
+yarn unlink @opentranslate/tencent
+yarn unlink @opentranslate/youdao
